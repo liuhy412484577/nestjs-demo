c65c967d3b7f8de93e5f1125a7eb594324043dc5
liuhaiyang <liuhaiyang@sonorus.network>
2023-12-11 14:43:23 +0800

    fix: prod env path

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 console.debug('==NODE_ENV===', process.env.NODE_ENV);
 const getEnvFilePath = () => {
     const localEnv = path.resolve('.env');
-    const prodEnv = path.resolve('.prod.env');
+    const prodEnv = path.resolve('.env.prod');
     if (!fs.existsSync(localEnv) && !fs.existsSync(prodEnv)) {
         throw new Error('缺少环境配置文件');
     }
